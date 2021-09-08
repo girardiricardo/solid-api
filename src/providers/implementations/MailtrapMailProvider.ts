40fa8ac5870c2c1f92a673637954a88db4ec4c1d
Ricardo da Silva Girardi <girardiricardo@outlook.com>
2021-09-08 19:57:19 -0300

    Adding mail implementation

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer';
+
+import { IMailProvider, IMessage } from "../IMailProvider";
+
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+  
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: parseInt(process.env.MAIL_PORT),
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    })
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body
+    })
+  }
+}
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
   constructor(
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Admin',
+        email: 'admin@app.com',
+      },
+      subject: 'Welcome to the app.',
+      body: '<p>You already can sign in into our app.</p>'
+    });
   }
 }
