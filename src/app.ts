9ee7fb338ac8e6edc73380fa9eb207b93c7385d7
Ricardo da Silva Girardi <girardiricardo@outlook.com>
2021-09-08 20:17:08 -0300

    Adding create user controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(router)
 
 export { app }
